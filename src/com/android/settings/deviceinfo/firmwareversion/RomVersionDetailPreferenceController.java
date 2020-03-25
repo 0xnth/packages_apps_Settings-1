ajithzres/packages_apps_Settings

Settings: Show build codename
Signed-off-by: Akito Mizukito <akito@evolution-x.org>
Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/android/settings/deviceinfo/firmwareversion/RomVersionDetailPreferenceController.java
@@ -36,6 +36,7 @@ public class RomVersionDetailPreferenceController extends BasePreferenceControll
     private static final String TAG = "romDialogCtrl";
     private static final String KEY_ROM_VERSION_PROP = "org.evolution.build_version";
     private static final String KEY_ROM_RELEASETYPE_PROP = "org.evolution.build_type";
+    private static final String KEY_ROM_CODENAME_PROP = "org.evolution.build_codename";
     private final PackageManager mPackageManager = this.mContext.getPackageManager();
 
     public RomVersionDetailPreferenceController(Context context, String preferenceKey) {
@@ -51,8 +52,10 @@ public class RomVersionDetailPreferenceController extends BasePreferenceControll
                 this.mContext.getString(R.string.device_info_default));
         String romReleasetype =  SystemProperties.get(KEY_ROM_RELEASETYPE_PROP,
                 this.mContext.getString(R.string.device_info_default));
+        String romCodename =  SystemProperties.get(KEY_ROM_CODENAME_PROP,
+                this.mContext.getString(R.string.device_info_default));
         if (!romVersion.isEmpty() && !romReleasetype.isEmpty())
-            return romVersion + " | " + romReleasetype;
+            return romVersion + " | " + romCodename + " | " + romReleasetype;
         else
             return mContext.getString(R.string.rom_version_default);
     }

0xnth/packages_apps_Settings-1

src/com/android/settings/deviceinfo/firmwareversion/RomVersionDetailPreferenceController.java
@@ -32,10 +32,11 @@ import com.android.settings.core.BasePreferenceController;
 
 public class RomVersionDetailPreferenceController extends BasePreferenceController {
 
-    private static final Uri INTENT_URI_DATA = Uri.parse("https://paypal.me/");
+    private static final Uri INTENT_URI_DATA = Uri.parse("https://sourceforge.net/projects/atom-os-project/files/");
     private static final String TAG = "romDialogCtrl";
     private static final String KEY_ROM_VERSION_PROP = "org.atom.build_version";
     private static final String KEY_ROM_RELEASETYPE_PROP = "org.atom.build_type";
+   private static final String KEY_ROM_CODENAME_PROP = "org.atom.build_codename";
     private final PackageManager mPackageManager = this.mContext.getPackageManager();
 
     public RomVersionDetailPreferenceController(Context context, String preferenceKey) {
@@ -51,8 +52,10 @@ public class RomVersionDetailPreferenceController extends BasePreferenceControll
                 this.mContext.getString(R.string.device_info_default));
         String romReleasetype =  SystemProperties.get(KEY_ROM_RELEASETYPE_PROP,
                 this.mContext.getString(R.string.device_info_default));
+        String romCodename =  SystemProperties.get(KEY_ROM_CODENAME_PROP,
+                this.mContext.getString(R.string.device_info_default));
         if (!romVersion.isEmpty() && !romReleasetype.isEmpty())
-            return romVersion + " | " + romReleasetype;
+            return romVersion + " | " + romCodename + " | " + romReleasetype;
         else
             return mContext.getString(R.string.rom_version_default);
     }